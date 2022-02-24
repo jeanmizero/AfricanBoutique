742db9ff6573ce6f2112406464d2b4f5796c2ba2
Jean Mizero <mizero2@gmail.com>
2022-02-24 00:43:09 -0700

    Add React-Bootstrap to the frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,23 @@
-import React from "react";
+import React from 'react'
+// Import Container
+import { Container } from 'react-bootstrap'
+// Import components
+import Header from './components/Header'
+import Footer from './components/Footer'
 
 // Main App
-const App =() =>{
-    return ( 
-      <>
-        <h1>Welcome to African Boutique</h1>
-      </>
-    );
+const App = () => {
+  return (
+    <>
+      <Header />
+      <main className='py-3'>
+        <Container>
+          <h1>Welcome to African Boutique</h1>
+        </Container>
+      </main>
+      <Footer />
+    </>
+  )
 }
 
-export default App;
+export default App
